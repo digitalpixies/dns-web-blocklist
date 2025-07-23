7e6c24f3ce171cd115b8a197a897cefde6bc11de
Robert Huie <rhuie@digitalpixies.com>
2025-07-22 19:40:22 -0700

    Replace 'request' with 'axios' in dependencies for improved HTTP requests

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const request = require('request');
+const axios = require('axios');
 const cheerio = require('cheerio');
 const url = 'https://raw.githubusercontent.com/WaLLy3K/wally3k.github.io/master/index.html';
 const fs = require('fs');
@@ -24,9 +24,11 @@ const ListToBeMergedIntoBlacklist = (classNames) => {
 
 const GetBlacklist = (url, filteredLines) => {
     return new Promise((resolve, reject) => {
-        request(url, {
+        axios.get(url, {
             timeout: 3000
-        }, (error, response, body) => {
+        })
+        .then(response => {
+            let body = response.data;
             let lines = body.trim().replace(/\r/g, '\n').replace(/\t/g, ' ').replace(/\ +/g, ' ').split('\n');
             for (let line of lines) {
                 if (line.includes('#')) {
@@ -37,7 +39,6 @@ const GetBlacklist = (url, filteredLines) => {
                 //skip ipv6 entries
                 if (line.includes(':'))
                     continue;
-                //console.log(line);
                 let parts = line.split(' ');
                 if (parts.length === 1)
                     filteredLines[parts[0]] = null;
@@ -49,33 +50,38 @@ const GetBlacklist = (url, filteredLines) => {
                 }
             }
             resolve();
+        })
+        .catch(error => {
+            reject(error);
         });
     });
 }
 
-request(url, {
+axios.get(url, {
     timeout: 3000
-}, (error, response, body) => {
-    if (!error) {
-        const $ = cheerio.load(body);
-        let blackListEntries = {};
-        let blacklistURLs = [];
-        let promises = [];
-        $('ul.bdUrlList').each((index, dom) => {
-            let classNames = $(dom).attr('class').split(' ');
-            if (ListToBeMergedIntoBlacklist(classNames)) {
-                $(".bdTick", dom).each((index, dom) => {
-                    let links = $('a', dom);
-                    blacklistURLs.push($(links[1]).attr('href'));
-                    promises.push(GetBlacklist($(links[1]).attr('href'), blackListEntries));
-                });
-            }
-        });
+})
+.then(response => {
+    const $ = cheerio.load(response.data);
+    let blackListEntries = {};
+    let blacklistURLs = [];
+    let promises = [];
+    $('ul.bdUrlList').each((index, dom) => {
+        let classNames = $(dom).attr('class').split(' ');
+        if (ListToBeMergedIntoBlacklist(classNames)) {
+            $(".bdTick", dom).each((index, dom) => {
+                let links = $('a', dom);
+                blacklistURLs.push($(links[1]).attr('href'));
+                promises.push(GetBlacklist($(links[1]).attr('href'), blackListEntries));
+            });
+        }
+    });
 
-        Promise.all(promises).then(() => {
-            for (let entry in blackListEntries) {
-                console.log(`0.0.0.0 ${entry}`);
-            }
-        });
-    }
+    Promise.all(promises).then(() => {
+        for (let entry in blackListEntries) {
+            console.log(`0.0.0.0 ${entry}`);
+        }
+    });
+})
+.catch(error => {
+    console.error('Error fetching data:', error);
 });
